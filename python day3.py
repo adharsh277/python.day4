b198d67b13a58ac33fa0f2a412afc6d544c65374
adharsh277 <114822057+adharsh277@users.noreply.github.com>
2022-10-19 21:58:28 +0530

    Add files via upload

---
+++ python day3.py
@@ -0,0 +1,56 @@
+from calendar import c
+from re import A
+from tkinter import Y
+
+
+x = 30
+y = 67
+z = x+y
+print(z)
+A = "i thought i was the one who is going to protect the world but for real am not"
+B = "its humans itself"
+c = "why people have so much confidence when they are good at something "
+d = "so now it time for unleashing the beast"
+print(A+B+c+d)
+myname = 'john samuel umitti is not goona good at you'
+print(myname+"but richard is  gonna create so ething you  can't feel")
+x, y = 90, 765
+print(x*22)
+print(x*9)
+print(x/55)
+# now it s for modifictaion
+x = "sometimes we think is not goona happen but what we do is gonna a change our future"
+print(x[-1:])
+print(x[1:])
+print(x[-9:-9])
+print(x[-5:8])
+print(x[8:-1])
+print(x.upper())
+print(x.lower())
+print(x.split())
+y = "You just have to decide what kind of man you want to grow up to be humans Whoever that man is he's going to change the world "
+print(x.upper())
+print(x.lower())
+print(x.split())
+print(x.replace('e', 'y'))
+print(x.replace('t', 'o'))
+print(x.replace('t', 'p'))
+print(x[1:])
+# now its time for the booleans
+print(bool(False))
+print(bool(True))
+# arithemetic operator
+print(10+381+67+80+817)
+print(10+38+67+874)
+print(10+67+80+87)
+print(138+675+80+585)
+# just my creative
+print(int(input()))
+print(x)
+x = "good night"
+y = 'day dreamers'
+print(x+y)
+# hyhj
+x = "hjki"
+y = ""
+print(x+y)
